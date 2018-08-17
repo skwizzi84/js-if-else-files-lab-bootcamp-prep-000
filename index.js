6d4af446964bf361fbe34317eaecc062869315ce
stephanie.kopet@cuw.edu <stephanie.kopet@cuw.edu>
2018-08-17 16:03:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (name==="Susan"){
   console.log("Expected the name to be Susan")
 }
 if (height==="70"){
-  console.log ("The name is correct")
+  console.log ("The height is correct")
 }else{
   console.log("Expected 70")
 }
